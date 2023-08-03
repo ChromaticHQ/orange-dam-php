af623095e4c3a89c24cfb4045ecc6e92b9d48070
Adam Zimmermann <adam@chromatichq.com>
2023-08-03 18:30:19 -0500

    Fix linting issues.

--- src/DataTableIdentifiers.php
+++ src/DataTableIdentifiers.php
@@ -7,11 +7,12 @@ namespace Chromatic\OrangeDam;
  *
  * @see https://example.orangelogic.com/API/DataTable/v2.2/Documents.All:Read
  */
-enum DataTableIdentifiers: string {
-  case SYSTEM_ID = 'CoreField.Identifier';
-  case RECORD_ID = 'RecordID';
-  case CLIENT_ID = 'CoreField.Id_Client';
-  CASE OTHER_NUMBER = 'CoreField.OtherNum';
-  CASE ORIGINAL_FILE_NAME = 'CoreField.OriginalFileName';
-  CASE ORIGINAL_VERSION = 'CoreField.DO_OriginalVersion';
+enum DataTableIdentifiers: string
+{
+    case SYSTEM_ID = 'CoreField.Identifier';
+    case RECORD_ID = 'RecordID';
+    case CLIENT_ID = 'CoreField.Id_Client';
+    case OTHER_NUMBER = 'CoreField.OtherNum';
+    case ORIGINAL_FILE_NAME = 'CoreField.OriginalFileName';
+    case ORIGINAL_VERSION = 'CoreField.DO_OriginalVersion';
 }
